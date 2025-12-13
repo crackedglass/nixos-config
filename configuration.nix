559aa35557ba995c0c59af6dc3965a6573d033c0
crackedglass <bunin.basil@yandex.ru>
2025-12-13 13:01:56 +0300

    Add power managment tools

--- configuration.nix
+++ configuration.nix
@@ -41,7 +41,7 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
-  services.power-profiles-daemon.enable = true;
+  # services.power-profiles-daemon.enable = true;
   services.upower.enable = true;
 
   security.rtkit.enable = true;
@@ -49,6 +49,28 @@
     enable = true;
     alsa.enable = true;
     pulse.enable = true;
+  };
+
+  powerManagement.powertop.enable = true;
+  services.thermald.enable = true;
+  services.system76-scheduler.settings.cfsProfiles.enable = true;
+
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+      CPU_HWP_DYN_BOOST_ON_AC = 1;
+      CPU_HWP_DYN_BOOST_ON_BAT = 0;
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      PLATFORM_PROFILE_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_BAT = "low-power";
+      START_CHARGE_THRESH_BAT0 = 75;
+      STOP_CHARGE_THRESH_BAT0 = 81;
+    };
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
