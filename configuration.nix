e0a2b29e26df411f456386e1c49298307c0fb520
crackedglass <bunin.basil@yandex.ru>
2025-12-13 13:31:43 +0300

    Power up mode on battery, because chrome became laggy

--- configuration.nix
+++ configuration.nix
@@ -64,9 +64,9 @@
       CPU_HWP_DYN_BOOST_ON_BAT = 0;
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-      PLATFORM_PROFILE_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "balance-performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance-power";
+      PLATFORM_PROFILE_ON_AC = "balance-performance";
       PLATFORM_PROFILE_ON_BAT = "low-power";
       START_CHARGE_THRESH_BAT0 = 75;
       STOP_CHARGE_THRESH_BAT0 = 81;
